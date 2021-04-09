909eda7f557d8220ada5c21933e65d53f7bf6d10
pminhyung <jasonpark9312@gmail.com>
2021-04-10 02:09:03 +0900

    Refactor main function

--- test_main.py
+++ test_main.py
@@ -1,11 +1,62 @@
 # import
+from IPython.display import display
+from glob import glob
+from tqdm import tqdm
 
-# main
-def main():
-    pass
+from tensorflow.keras import models
+from tensorflow.keras.preprocessing.image import load_img
+from playsound import playsound
+
+from test_helper import load_prep_img
 
 
+def main(config):
+
+    test_dir = config['test_dir']
+
+    categories = ['safe', 'danger']
+
+    # load images
+    img_paths = glob(test_dir + '/*.bmp') # need update to cv2 video frames
+
+    for path in tqdm(img_paths):
+        image = load_img(path)
+        display(image)
+        image_arr_dm4 = load_prep_img(path)
+
+    # Showing if the baby's in safe or danger
+    if 'eat' in path[:-3]:
+        model = models.load_model('pretrained_model/Baby_eat.h5')  # load pretrained model
+        categories = ['difficult..', 'danger', 'safe']
+    elif 'door' in path[:-3]:
+        model = models.load_model('pretrained_model/Baby_door.h5')
+        categories = ['difficult..', 'danger', 'safe']
+    elif 'fall' in path[:-3]:
+        model = models.load_model('pretrained_model/Baby_fall.h5')
+        categories = ['difficult..', 'danger', 'safe']
+    elif 'kitchen' in path[:-3]:
+        model = models.load_model('pretrained_model/Baby_kitchen.h5')
+        categories = ['difficult..', 'safe', 'danger']
+    else:
+        print('사진에 적합한 모델이 없습니다.')
+
+    predict = model.predict_classes(image_arr_dm4)
+    answer = categories[int(predict)]
+    print(answer)
+
+    # Playing Mom's voice in each situation, Mom and baby hears it immediately
+    if ('eat' in path[:-3]) & (answer == 'danger'):
+        return playsound('EatWarning.mp3')
+    elif ('door' in path[:-3]) & (answer == 'danger'):
+        return playsound('DoorWarning.mp3')
+    elif ('fall' in path[:-3]) & (answer == 'danger'):
+        return playsound('FallWarning.mp3')
+    elif ('kitchen' in path[:-3]) & (answer == 'danger'):
+        return playsound('FallWarning.mp3')
 
 
 if __name__ == '__main__':
-    main()
+    # argparse
+
+    # run test
+    main(config)
--- test_sample.py
+++ test_sample.py
@@ -1,38 +1,60 @@
+import os
 from glob import glob
+
 from tqdm import tqdm
 import numpy as np
+import matplotlib.pyplot as plt
+
 from tensorflow.keras.models import load_model
-from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+
 from test_helper import load_prep_img
-from models import pretrained_baby_CNN
 
-config = {
-'IM_WIDTH': 500,
-'IM_HEIGHT': 275,
-'class_num' : 2,
-'n_block' : 5,
-'kernel_size' : (3, 3),
-'pool_size' : (2, 2),
-'n_filters' : [32, 64, 128],
-'n_dense_hidden' : 1024,
-'dropout_conv' : 0.3,
-'dropout_dense' : 0.3
-}
 
-# load test_data
-img_paths = glob('test_samples/*.bmp')
+def test_sample(config):
+    """test sample images with pretrained model
 
-# load model
-model = load_model('pretrained_model.h5')
-#model = pretrained_baby_CNN(config) # pretrained_model
-#model = model.load_weights()
+    requirement: locate sample files at "test_samples" directory
 
-# load test images and predict
-for path in tqdm(img_paths):
-    img_arr4 = load_prep_img(path)
-    yhat = model.predict(img_arr4)
-    idx = np.argmax(yhat[0])
-    print('%s (%.2f%%)' % (custom_labels[idx], yhat[0][idx] * 100))
+    """
+    labels = config['general']['labels']
+
+    # load test_data
+    img_paths = glob('test_samples/*.bmp')
+
+    # load model
+    model = load_model('pretrained_model.h5')
+    #model = Pretrained_baby_CNN(config) # pretrained_model
+    #model = model.load_weights()
+
+    # load test images and predict
+    for path in tqdm(img_paths):
+        img_arr4 = load_prep_img(path)
+        yhat = model.predict(img_arr4)
+        idx = np.argmax(yhat[0])
+
+        plt.imshow(load_img(path))
+        plt.title(os.path.basename(path))
+        print('%s (%.2f%%)' % (labels[idx], yhat[0][idx] * 100))
 
 if __name__ == '__main__':
-    pass
+
+    config = {'general': {'labels': ['difficult..','safe','danger'],
+                          'img_w': 500,
+                          'img_h': 275,
+                          'train_dir': 'videos/images/1/train/',
+                          'test_dir': 'videos/images/1/train/',
+                          'train_csv_dir': './csvdata',
+                          'test_csv_dir': './csvdata'
+                          },
+              'model': {'n_block': 5,
+                        'kernel_size': (3, 3),
+                        'pool_size': (2, 2),
+                        'n_filters': [32, 64, 128],
+                        'n_dense_hidden': 1024,
+                        'dropout_conv': 0.3,
+                        'dropout_dense': 0.3},
+              'train': {'learning_rate': 0.001, 'batch_size': 256}
+              }
+
+    test_sample(config)
